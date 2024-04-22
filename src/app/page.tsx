7004e90e805532480736043d7fe157b1759208be
chihiro_imac <ppgeswimmer48de@icloud.com>
2024-04-22 23:22:46 +0900

    240422_publicフォルダ内のファイル一覧を取得させた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
 export default function Home() {
+  const path = require('path')
+  const fs = require('fs');
+  const pagetxtPath: string = path.join(__dirname, '../../../public/pagetxt')
+  const files: string[] = fs.readdirSync(pagetxtPath);
   return (
-    <div>Hello,world</div>
+    <div>
+      <h1>みだし</h1>
+      <p>{files.join(' ')}</p>
+      <p>publicフォルダ内のファイル名を取得したので</p>
+      <p>ファイル名からファイルの中身を読んで</p>
+      <p>いい感じに表示するコンポーネントを作る</p>
+      <p>適当に枠つけて繰り返す</p>
+    </div>
   );
 }
